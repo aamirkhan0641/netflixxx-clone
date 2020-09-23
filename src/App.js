507a23493520dd22a168e74729aa0a66443a1ce1
Aamir <aamir.khan0641@gmail.com>
2020-09-23 20:09:04 +0400

    solving error

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from "react";
 import "./App.css";
-import Row from "./Row.js";
-import request from "./requesst";
-import requests from "./requesst";
+import Row from "./Row";
+import request from "./requests";
+import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
-      <h1>Let's Go Aamir Build the Netflix Clone </h1>
+      <h1>Let's Go Build the Netflix Clone </h1>
       <Row
         title="NETFLIX ORIGINALS"
         fetchUrl={requests.fetchNetflixOriginals}
       />
       <Row title="TRENDING NOW " fetchUrl={request.fetchTrending} />
+      <Row title="Top Rated" fetchURL={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchURL={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchURL={requests.fetchRomanceMovies} />
+      <Row
+        title="Documentaries Movies"
+        fetchURL={requests.fetchDocumentaries}
+      />
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -25,6 +25,7 @@ function Row({ title, fetchUrl }) {
         {/* several rp__poster(s)*/}
         {movies.map((movie) => (
           <img
+            key={movie.id}
             className="row__poster"
             src={`${base_url}${movie.poster_path}`}
             alt={movie.name}
